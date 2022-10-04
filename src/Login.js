dc066ca4611f273ad74062386bb142c9eb4951d3
Roy Lee <leeroy0912@gmail.com>
2022-10-04 14:52:31 -0400

    attempt to fix the login

--- src/Login.js
+++ src/Login.js
@@ -20,7 +20,14 @@ const Login = (props) => {
       body: JSON.stringify(user),
     }).then((r) => r.json());
 
-    props.onSignupSubmit(user);
+    fetch("http://localhost:9292/current-user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    }).then((r) => r.json());
+
     navigate("/create-account");
   }
 
